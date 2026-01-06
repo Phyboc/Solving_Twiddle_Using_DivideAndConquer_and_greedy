a381d77138f35d2e71fde23fa2378d58abd76e53
Phyboc <sivasubramanikj@gmail.com>
2026-01-07 00:55:13 +0530

    Changed the possible orientation from 2x2 to 3x3

--- test.java
+++ test.java
@@ -1,13 +1,15 @@
 import java.util.*;
 
 public class test {
+
     public static class cube {
         int start1;
         int end1;
         int start2;
         int end2;
         int difference;
-        public cube(int start1, int end1, int start2, int end2, int difference){
+
+        public cube(int start1, int end1, int start2, int end2, int difference) {
             this.start1 = start1;
             this.end1 = end1;
             this.start2 = start2;
@@ -16,43 +18,46 @@ public class test {
         }
     }
 
-    // Tabu list storing 2x2 blocks as strings
-    static Set<String> tabuList = new HashSet<>();
+    static HashSet<String> tabuList = new HashSet<>(); // stores 3x3 submatrix states
 
     public static void main(String[] args) {
         int[][] matrix = {
-            {3,20,14,24,1},
-            {13,25,9,7,18},
-            {2,8,11,5,6},
-            {19,15,12,4,17},
-            {22,16,23,21,10}
+            {3, 20, 14, 24, 1},
+            {13, 25, 9, 7, 18},
+            {2, 8, 11, 5, 6},
+            {19, 15, 12, 4, 17},
+            {22, 16, 23, 21, 10}
         };
+
+        int[][] finalMatrix = getSortedMatrix(matrix);
 
         boolean choice = true;
         Scanner sc = new Scanner(System.in);
 
-        while (choice && !isSorted(matrix)){
-            ArrayList<cube> cubes = new ArrayList<>(); 
-            ans(matrix,0,matrix.length,0,matrix[0].length,cubes);
+        while (choice && !isSorted(matrix)) {
+            ArrayList<cube> cubes = new ArrayList<>();
+            ans(matrix, 0, matrix.length, 0, matrix[0].length, cubes, finalMatrix);
 
-            // Sort by difference (smaller is better)
+            // Sort cubes by difference (smaller difference is better)
             cubes.sort(Comparator.comparingInt(a -> a.difference));
 
             boolean moveMade = false;
             for (cube c : cubes) {
-                if (checkAndAdd(matrix, c.start1, c.start2)) {
-                    rotate(matrix, c.start1, c.end1, c.start2, c.end2);
+                if (!checkAndAdd(matrix, c.start1, c.start2)) {
+                    // rotate 3x3 and mark as used
+                    rotate3x3(matrix, c.start1, c.end1, c.start2, c.end2);
                     moveMade = true;
                     break;
                 }
             }
 
-            // Undo if no move was possible
+            // If no move was made, undo rotations (rotate 3 more times to restore)
             if (!moveMade) {
-                // Rotate first cube 3 more times to restore (since it's 2x2)
-                rotate(matrix, cubes.get(0).start1, cubes.get(0).end1, cubes.get(0).start2, cubes.get(0).end2);
-                rotate(matrix, cubes.get(0).start1, cubes.get(0).end1, cubes.get(0).start2, cubes.get(0).end2);
-                rotate(matrix, cubes.get(0).start1, cubes.get(0).end1, cubes.get(0).start2, cubes.get(0).end2);
+                for (cube c : cubes) {
+                    rotate3x3(matrix, c.start1, c.end1, c.start2, c.end2);
+                    rotate3x3(matrix, c.start1, c.end1, c.start2, c.end2);
+                    rotate3x3(matrix, c.start1, c.end1, c.start2, c.end2);
+                }
             }
 
             matrixprint(matrix);
@@ -64,87 +69,102 @@ public class test {
         matrixprint(matrix);
     }
 
-    // Check if fully sorted
-    private static boolean isSorted(int[][] matrix){
+    // generate the final sorted matrix
+    private static int[][] getSortedMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[] arr = new int[n * m];
+        int idx = 0;
+        for (int[] row : matrix) {
+            for (int val : row) arr[idx++] = val;
+        }
+        Arrays.sort(arr);
+        int[][] sorted = new int[n][m];
+        idx = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                sorted[i][j] = arr[idx++];
+            }
+        }
+        return sorted;
+    }
+
+    private static boolean isSorted(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
         int prev = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
                 if (matrix[i][j] < prev) return false;
-                prev = matrix[i][j];
-            }
-        }
+                else prev = matrix[i][j];
         return true;
-    }   
+    }
 
-    // Print matrix
-    private static void matrixprint(int[][] matrix){
-        for (int i = 0; i < matrix.length; i++){
-            for (int j = 0; j < matrix[0].length; j++){
-                System.out.print(matrix[i][j] + " ");
-            }
+    private static void matrixprint(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) System.out.print(val + " ");
             System.out.println();
         }
     }
 
-    // Generate all 2x2 cubes
-    private static void ans(int[][] matrix, int start1, int end1, int start2, int end2, ArrayList<cube> cubes){
-        if ((end1 - start1) == 2 && (end2 - start2) == 2){
-            cubes.add(new cube(start1, end1, start2, end2, find(matrix, start1, end1, start2, end2)));
+    private static void ans(int[][] matrix, int start1, int end1, int start2, int end2, ArrayList<cube> cubes, int[][] finalMatrix) {
+        if (end1 - start1 == 3 && end2 - start2 == 3) {
+            cubes.add(new cube(start1, end1, start2, end2, find(matrix, start1, end1, start2, end2, finalMatrix)));
             return;
         }
-        if ((end1 - start1) > 2 && (end2 - start2) > 2) {
-            ans(matrix, start1, end1 - 1, start2, end2-1, cubes);
-            ans(matrix, start1, end1 -1, start2+1,end2, cubes);
-            ans(matrix, start1 +1, end1, start2, end2 -1, cubes);
-            ans(matrix, start1 + 1, end1, start2 +1, end2, cubes);
-        }
+
+        if (end1 - start1 < 3 || end2 - start2 < 3) return;
+
+        ans(matrix, start1, end1 - 1, start2, end2 - 1, cubes, finalMatrix);
+        ans(matrix, start1, end1 - 1, start2 + 1, end2, cubes, finalMatrix);
+        ans(matrix, start1 + 1, end1, start2, end2 - 1, cubes, finalMatrix);
+        ans(matrix, start1 + 1, end1, start2 + 1, end2, cubes, finalMatrix);
     }
 
-    // Calculate global difference after rotating 2x2 block
-    private static int difference(int[][] matrix, int start1, int end1, int start2, int end2){
-        // Copy matrix and rotate 2x2 block
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int[][] copy = new int[n][m];
-        for (int i = 0; i < n; i++)
-            System.arraycopy(matrix[i], 0, copy[i], 0, m);
+    private static int find(int[][] matrix, int start1, int end1, int start2, int end2, int[][] finalMatrix) {
+        int[][] copy = new int[3][3];
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                copy[i][j] = matrix[start1 + i][start2 + j];
 
-        rotate(copy, start1, end1, start2, end2);
-
-        // Count inversions in full matrix
-        int count = 0;
-        int prev = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (copy[i][j] < prev) count++;
-                prev = copy[i][j];
-            }
-        }
-        return count;
+        rotate3x3(copy, 0, 3, 0, 3); // rotate copy
+        return difference(copy, finalMatrix, start1, start2);
     }
 
-    // Copy 2x2 block into string for tabu list
-    private static boolean checkAndAdd(int[][] matrix, int start1, int start2){
-        String key = matrix[start1][start2] + "," + matrix[start1][start2+1] + "," +
-                     matrix[start1+1][start2] + "," + matrix[start1+1][start2+1];
-        if (tabuList.contains(key)) return false;
+    // difference compared with final matrix
+    private static int difference(int[][] block, int[][] finalMatrix, int start1, int start2) {
+        int diff = 0;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (block[i][j] != finalMatrix[start1 + i][start2 + j]) diff++;
+        return diff;
+    }
+
+    private static void rotate3x3(int[][] mat, int start1, int end1, int start2, int end2) {
+        // rotate outer corners
+        int temp = mat[start1][start2];
+        mat[start1][start2] = mat[start1 + 2][start2];
+        mat[start1 + 2][start2] = mat[start1 + 2][start2 + 2];
+        mat[start1 + 2][start2 + 2] = mat[start1][start2 + 2];
+        mat[start1][start2 + 2] = temp;
+
+        // rotate inner edges
+        temp = mat[start1][start2 + 1];
+        mat[start1][start2 + 1] = mat[start1 + 1][start2];
+        mat[start1 + 1][start2] = mat[start1 + 2][start2 + 1];
+        mat[start1 + 2][start2 + 1] = mat[start1 + 1][start2 + 2];
+        mat[start1 + 1][start2 + 2] = temp;
+    }
+
+    // check tabu list and store
+    private static boolean checkAndAdd(int[][] matrix, int start1, int start2) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                sb.append(matrix[start1 + i][start2 + j]).append(",");
+        String key = sb.toString();
+        if (tabuList.contains(key)) return true; // already visited
         tabuList.add(key);
-        return true;
-    }
-
-    // Rotate 2x2 block
-    private static void rotate(int[][] graph, int start1, int end1, int start2, int end2){
-        int temp = graph[start1][start2];
-        graph[start1][start2] = graph[start1][end2-1];
-        graph[start1][end2-1] = graph[end1-1][end2-1];
-        graph[end1-1][end2-1] = graph[end1-1][start2];
-        graph[end1-1][start2] = temp;
-    }
-
-    // Copy 2x2 block and calculate difference
-    private static int find(int[][] matrix, int start1, int end1, int start2, int end2){
-        return difference(matrix, start1, end1, start2, end2);
+        return false;
     }
 }
