861476f50daf7a6150aab098453d8b8428c8061f
Phyboc <sivasubramanikj@gmail.com>
2026-01-07 00:10:23 +0530

    Second Draft

--- test.java
+++ test.java
@@ -1,14 +1,11 @@
-
 import java.util.*;
 
 public class test {
-    public static class cube {
-        int start1;
-        int end1;
-        int start2;
-        int end2;
-        int difference;
-        public cube(int start1, int end1, int start2, int end2, int difference){
+
+    public static class Cube {
+        int start1, end1, start2, end2, difference;
+
+        public Cube(int start1, int end1, int start2, int end2, int difference) {
             this.start1 = start1;
             this.end1 = end1;
             this.start2 = start2;
@@ -16,90 +13,129 @@ public class test {
             this.difference = difference;
         }
     }
+
+    // Tabu list stores small 2x2 rotations applied
+    private static final List<String> tabuList = new ArrayList<>();
+
     public static void main(String[] args) {
         int[][] matrix = {
-            {3,20,14,24,1},
-            {13,25,9,7,18},
-            {2,8,11,5,6},
-            {19,15,12,4,17},
-            {22,16,23,21,10}
+            {3, 20, 14, 24, 1},
+            {13, 25, 9, 7, 18},
+            {2, 8, 11, 5, 6},
+            {19, 15, 12, 4, 17},
+            {22, 16, 23, 21, 10}
         };
-        boolean choice = true;
+
         Scanner sc = new Scanner(System.in);
-        while (choice && !isSorted(matrix)){
-            ArrayList<cube> cubes = new ArrayList<>(); 
-            ans(matrix,0,matrix.length,0,matrix.length,cubes);
-            cubes.sort(Comparator.comparingInt(a -> a.difference));      
-            rotate(matrix, cubes.get(0).start1, cubes.get(0).end1, cubes.get(0).start2, cubes.get(0).end2);
-            matrixprint(matrix);
+        boolean choice = true;
+
+        while (choice && !isSorted(matrix)) {
+            ArrayList<Cube> cubes = new ArrayList<>();
+            ans(matrix, 0, matrix.length, 0, matrix[0].length, cubes);
+
+            // Sort cubes by difference (best first)
+            cubes.sort(Comparator.comparingInt(a -> a.difference));
+
+            boolean moveMade = false;
+            for (Cube c : cubes) {
+                rotate(matrix, c.start1, c.end1, c.start2, c.end2);
+                if (!checkAndAdd(matrix, c.start1, c.start2)) {
+                    // Move already seen, undo by rotating 3 more times
+                    for (int i = 0; i < 3; i++) {
+                        rotate(matrix, c.start1, c.end1, c.start2, c.end2);
+                    }
+                } else {
+                    moveMade = true;
+                    break; // accept first unseen move
+                }
+            }
+
+            matrixPrint(matrix);
             System.out.println("Once more? (1/0)");
             choice = (sc.nextInt() == 1);
         }
+
         System.out.println("Sorted Matrix:");
-        matrixprint(matrix);
+        matrixPrint(matrix);
     }
-    
-    private static boolean isSorted(int[][] matrix){
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int prev = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < m; j++){
-                if (matrix[i][j] < prev){
-                    return false;
-                }
-                prev = matrix[i][j];
-            }
-        }
-        return true;
-    }   
-    private static void matrixprint(int[][] matrix){
-        for (int i = 0; i < matrix.length; i++){
-            for (int j = 0; j < matrix[0].length; j++){
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-    private static void ans(int[][] matrix, int start1, int end1, int start2, int end2, ArrayList<cube> cubes){
-        if ((end1 - start1) == 2 && (end2 - start2) == 2){
-            cubes.add(new cube(start1, end1, start2, end2, find(matrix, start1, end1, start2, end2)));
+
+    // Generate all 2x2 subcubes
+    private static void ans(int[][] matrix, int start1, int end1, int start2, int end2, ArrayList<Cube> cubes) {
+        if ((end1 - start1) == 2 && (end2 - start2) == 2) {
+            cubes.add(new Cube(start1, end1, start2, end2, find(matrix, start1, end1, start2, end2)));
             return;
         }
-        ans(matrix, start1, end1 - 1, start2, end2-1, cubes);
-        ans(matrix, start1, end1 -1, start2+1,end2, cubes);
-        ans(matrix, start1 +1, end1, start2, end2 -1, cubes);
-        ans(matrix, start1 + 1, end1, start2 +1, end2, cubes);
-
+        if (end1 - start1 > 2 && end2 - start2 > 2) {
+            ans(matrix, start1, end1 - 1, start2, end2 - 1, cubes);
+            ans(matrix, start1, end1 - 1, start2 + 1, end2, cubes);
+            ans(matrix, start1 + 1, end1, start2, end2 - 1, cubes);
+            ans(matrix, start1 + 1, end1, start2 + 1, end2, cubes);
+        }
     }
 
-    private static int difference(int[][] matrix, int start1, int end1, int start2, int end2){
+    // Count number of inversions in 2x2
+    private static int difference(int[][] matrix, int start1, int end1, int start2, int end2) {
         int count = 0;
-        if (matrix[start1][start2] > matrix[start1][end2-1]) count++;
-        if (matrix[start1][end2-1] > matrix[end1-1][start2]) count++;
-        if (matrix[end1-1][start2] > matrix[end1-1][end2-1]) count++;
+        if (matrix[start1][start2] > matrix[start1][end2 - 1]) count++;
+        if (matrix[start1][end2 - 1] > matrix[end1 - 1][start2]) count++;
+        if (matrix[end1 - 1][start2] > matrix[end1 - 1][end2 - 1]) count++;
         return count;
     }
-    private static int find(int[][] matrix, int start1, int end1, int start2, int end2){
 
-        int rows = end1 - start1;
-        int cols = end2 - start2;
-
-        int[][] copy = new int[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+    // Copy 2x2 submatrix and compute difference after rotation
+    private static int find(int[][] matrix, int start1, int end1, int start2, int end2) {
+        int[][] copy = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
                 copy[i][j] = matrix[start1 + i][start2 + j];
             }
         }
-        rotate(copy, 0, rows, 0, cols);
-        return difference(copy, 0, rows, 0, cols);
+        rotate(copy, 0, 2, 0, 2);
+        return difference(copy, 0, 2, 0, 2);
     }
-    private static void rotate(int[][] graph, int start1, int end1, int start2, int end2){
+
+    // Rotate 2x2 submatrix clockwise
+    private static void rotate(int[][] graph, int start1, int end1, int start2, int end2) {
         int temp = graph[start1][start2];
-        graph[start1][start2] = graph[start1][end2-1];
-        graph[start1][end2-1] = graph[end1-1][end2-1];
-        graph[end1-1][end2-1] = graph[end1-1][start2];
-        graph[end1-1][start2] = temp;
+        graph[start1][start2] = graph[start1][end2 - 1];
+        graph[start1][end2 - 1] = graph[end1 - 1][end2 - 1];
+        graph[end1 - 1][end2 - 1] = graph[end1 - 1][start2];
+        graph[end1 - 1][start2] = temp;
     }
-    
+
+    // Print matrix
+    private static void matrixPrint(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) System.out.print(val + " ");
+            System.out.println();
+        }
+    }
+
+    // Check if matrix is sorted
+    private static boolean isSorted(int[][] matrix) {
+        int prev = Integer.MIN_VALUE;
+        for (int[] row : matrix) {
+            for (int val : row) {
+                if (val < prev) return false;
+                prev = val;
+            }
+        }
+        return true;
+    }
+
+    // ---------------- Tabu list methods ----------------
+    private static String matrixKey(int[][] matrix, int x, int y) {
+        // Convert 2x2 starting at (x,y) to a string key
+        return matrix[x][y] + "," + matrix[x][y + 1] + "," + matrix[x + 1][y] + "," + matrix[x + 1][y + 1];
+    }
+
+    private static boolean checkAndAdd(int[][] matrix, int x, int y) {
+        String key = matrixKey(matrix, x, y);
+        if (tabuList.contains(key)) {
+            return false; // Already seen
+        }
+        tabuList.add(key);
+        if (tabuList.size() > 50) tabuList.remove(0); // Limit tabu list size
+        return true;
+    }
 }
